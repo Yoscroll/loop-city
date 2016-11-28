ef0a54661891e838d2e1313b55e12cfbc4d4c496
Yoscroll <ianhenry.c808@gmail.com>
2016-11-28 08:30:24 -1000

    finished first 3

---
+++ loopcity.js
@@ -0,0 +1,24 @@
+var count = null;
+for (count = 0;count<=100;count++){
+  console.log(count);
+}
+
+var animalCount=null;
+for (animalCount = 2; animalCount <=7; animalCount++){
+console.log("there are "+animalCount+" many animals in Noah's Arc");
+}
+
+var partyAnimals = ["pig","dog","cat","wolf","snail"];
+for (x = 0; x <=4; x++){
+  console.log(partyAnimals[x]);
+}
+
+var weaponChest = ["Mace","Sorcerer's Stone","Excalibur"];
+var hero{
+  name: "Merlin",
+  weaponChest: "Mace",
+  power:30
+};
+function something(par) {
+  
+}
