ca779b10d457dc04c6a15486e8bfa44aa5cbc05e
Yoscroll <ianhenry.c808@gmail.com>
2016-11-30 08:38:26 -1000

    add

--- loopcity.js
+++ loopcity.js
@@ -3,15 +3,18 @@ for (count = 0;count<=100;count++){
   console.log(count);
 }
 
+
 var animalCount=null;
 for (animalCount = 2; animalCount <7; animalCount++){
 console.log("there are "+animalCount+" many animals in Noah's Arc");
 }
 
+
 var partyAnimals = ["pig","dog","cat","wolf","snail"];
 for (x = 0; x <=4; x++){
   console.log(partyAnimals[x]);
 }
+
 
 var weaponChest = ["Mace","Sorcerer's Stone","Excalibur"];
 var hero={
@@ -19,13 +22,13 @@ var hero={
   weapon: "Mace",
   power:30
 };
-function something(par) {
-  if (hero.name=="Merlin"&&hero.weapon=="Sorcerer's Stone"){
-    console.log(hero.name+ " shall slay the dragon with the"+hero.weapon);
+function something(name,weapon) {
+  if (name=="Merlin"&&weapon=="Sorcerer's Stone"){
+    console.log(name+ " shall slay the dragon with the"+weapon);
   }
   else{
-    hero.weapon="Sorcerer's Stone";
-    console.log(hero.name+" is ready for war with the "+hero.weapon);
+    weapon=weaponChest[1];
+    console.log(name+" is ready for war with the "+weapon);
   }
 }
-something();
+something(hero.name,hero.weapon);
